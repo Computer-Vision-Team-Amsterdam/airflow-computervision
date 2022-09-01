c6d20faf2a0d5be9a2961e84a2871094144b2b73
Diana <epureanudiana13@gmail.com>
2022-09-01 14:52:20 +0200

    just pull container, no print

--- dags/dag.py
+++ dags/dag.py
@@ -47,8 +47,6 @@ with DAG(
         name="test-pullACR",
         task_id="diana_test",
         image="cvtweuacrogidgmnhwma3zq.azurecr.io/diana-test:latest",
-        cmds=["bash", "-cx"],
-        arguments=["echo Hello from container in ACR!"],
         namespace="airflow-cvision2",
         get_logs=True
     )
