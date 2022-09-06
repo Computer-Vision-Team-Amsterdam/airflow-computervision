4a3b3d607d4a04bf0133a7493ffc0bedd5a9b6cb
Diana <epureanudiana13@gmail.com>
2022-09-06 12:42:01 +0200

    back to python script

--- dags/dag.py
+++ dags/dag.py
@@ -34,8 +34,8 @@ with DAG(
         name="test-cloudvps-connection",
         task_id="test_cloudvps_connection",
         image="cvtweuacrogidgmnhwma3zq.azurecr.io/retrieve-images:latest",
-        cmds=["bash", "-cx"],
-        arguments=["echo Hello"],
+        cmds=["python"],
+        arguments=["retrieve_images.py"],
         namespace="airflow-cvision2",
         get_logs=True
     )
