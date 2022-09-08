5fcc1c866228d40f1ed8987c598c85c6e7236f8f
Diana <epureanudiana13@gmail.com>
2022-09-08 09:21:38 +0200

    only python operator

--- dags/dag.py
+++ dags/dag.py
@@ -1,15 +1,16 @@
 import os
-#import json
-#from typing import Optional
+import json
+import shutil
+from typing import Optional, Tuple
 
-#import requests
-#from requests.auth import HTTPBasicAuth
+import requests
+from requests.auth import HTTPBasicAuth
 
 from datetime import datetime, timedelta
 
-#from azure.identity import DefaultAzureCredential
-#from azure.keyvault.secrets import SecretClient
-
+from azure.identity import DefaultAzureCredential
+from azure.keyvault.secrets import SecretClient
+from pathlib import Path
 from airflow import DAG
 
 from airflow.operators.python import PythonOperator
@@ -18,27 +19,76 @@ from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import (
 )
 from common import MessageOperator
 
-container_vars = {"AZURE_CLIENT_ID": os.getenv("AZURE_CLIENT_ID")}
-all_variables = dict(os.environ)
-"""
+BASE_URL = f"https://3206eec333a04cc980799f75a593505a.objectstore.eu/intermediate/"
+
+airflow_secrets = json.loads(os.environ["AIRFLOW__SECRETS__BACKEND_KWARGS"])
+KVUri = airflow_secrets["vault_url"]
+
+credential = DefaultAzureCredential()
+client = SecretClient(vault_url=KVUri, credential=credential)
+
+username_secret = client.get_secret(name="CloudVpsRawUsername")
+password_secret = client.get_secret(name="CloudVpsRawPassword")
+
+USERNAME = username_secret.value
+PASSWORD = password_secret.value
+
+
+def split_pano_id(pano_id: str) -> Tuple[str, str]:
+    """
+    Splits name of the panorama in TMX* and pano*
+    """
+    id_name = pano_id.split("_")[0]
+    index = pano_id.index("_")
+    img_name = pano_id[index + 1:]
+    return id_name, img_name
+
+
+def download_panorama_from_cloudvps(
+        date: datetime, panorama_id: str, output_dir: Path = Path("./retrieved_images")
+) -> None:
+    """
+    Downloads panorama from cloudvps to local folder.
+    """
+
+    if Path(f"./{output_dir}/{panorama_id}.jpg").exists():
+        print(f"Panorama {panorama_id} is already downloaded.")
+        return
+    id_name, img_name = split_pano_id(panorama_id)
+
+    try:
+        url = (
+                BASE_URL
+                + f"{date.year}/{str(date.month).zfill(2)}/{str(date.day).zfill(2)}/{id_name}/{img_name}.jpg"
+        )
+
+        response = requests.get(
+            url, stream=True, auth=HTTPBasicAuth(USERNAME, PASSWORD)
+        )
+        if response.status_code == 404:
+            raise FileNotFoundError(f"No resource found at {url}")
+        filename = f"./{output_dir}/{panorama_id}.jpg"
+        with open(filename, "wb") as out_file:
+            shutil.copyfileobj(response.raw, out_file)
+        del response
+
+        print(f"{panorama_id} completed.")
+    except Exception as e:
+        print(f"Failed for panorama {panorama_id}:\n{e}")
+
+
 def test_connection_python():
 
-    airflow_secrets = json.loads(os.environ["AIRFLOW__SECRETS__BACKEND_KWARGS"])
-    KVUri = airflow_secrets["vault_url"]
+    pano_dates = [datetime(2016, 3, 17), datetime(2016, 3, 17), datetime(2016, 3, 17)]
+    pano_ids = [
+        "TMX7315120208-000020_pano_0000_000000",
+        "TMX7315120208-000020_pano_0000_000001",
+        "TMX7315120208-000020_pano_0000_000002",
+    ]
 
-    credential = DefaultAzureCredential()
-    client = SecretClient(vault_url=KVUri, credential=credential)
+    for pano_date, pano_id in zip(pano_dates, pano_ids):
+        download_panorama_from_cloudvps(pano_date, pano_id)
 
-    username_secret = client.get_secret(name="CloudVpsRawUsername")
-    password_secret = client.get_secret(name="CloudVpsRawPassword")
-
-
-    url = "https://3206eec333a04cc980799f75a593505a.objectstore.eu/intermediate/2016/03/17/TMX7315120208-000020/pano_0000_000000.jpg"
-    response = requests.get(
-        url, stream=True, auth=HTTPBasicAuth(username_secret.value, password_secret.value)
-    )
-    print(f"response code  is {response.status_code}")
-"""
 
 with DAG(
         "test",
@@ -62,13 +112,12 @@ with DAG(
     )
     """
 
-    """"
     test_connection_python = PythonOperator(
         task_id="test_connection_python",
         python_callable=test_connection_python
     )
-    """
 
+    """
     retrieve_images = KubernetesPodOperator(
         name="test-cloudvps-connection",
         task_id="test_cloudvps_connection",
@@ -93,10 +142,11 @@ with DAG(
         namespace="airflow-cvision2",
         get_logs=True
     )
+    """
 
-    #var_1 = test_connection_python
-    var_2 = retrieve_images
-    var_3 = retrieve_images_one_env
+    var_1 = test_connection_python
+    #var_2 = retrieve_images
+    #var_3 = retrieve_images_one_env
 
     # volume_mount = k8s_models.V1VolumeMount(
     #     name="dags-pv",
