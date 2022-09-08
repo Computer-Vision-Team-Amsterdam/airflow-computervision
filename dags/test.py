14d0ba66e03aa106bc4f8e1a4928109920741252
Diana <epureanudiana13@gmail.com>
2022-09-08 23:19:54 +0200

    uncommented another line

--- dags/test.py
+++ dags/test.py
@@ -15,11 +15,19 @@ airflow_secrets = json.loads(os.environ["AIRFLOW__SECRETS__BACKEND_KWARGS"])
 KVUri = airflow_secrets["vault_url"]
 print(f"KVURI is {KVUri}")
 
-
 client = SecretClient(vault_url=KVUri, credential=credential)
 
-"""
 username_secret = client.get_secret(name="CloudVpsRawUsername")
+try:
+    print(f"username is {json.loads(username_secret.value)}")
+except:
+    print("first way of printing is wrong")
+
+try:
+    print(f"username is {username_secret.value}")
+except:
+    print("second way of printing is wrong")
+"""
 password_secret = client.get_secret(name="CloudVpsRawPassword")
 
 socket.setdefaulttimeout(100)
