1ce3cde5e178d8966f08d7ccb8aff29897448075
C van Riel <c.van.riel@amsterdam.nl>
2022-09-13 13:09:20 +0200

    test MI issue

--- dags/always-on-dag.py
+++ dags/always-on-dag.py
@@ -19,7 +19,7 @@ from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import (
 AZURE_OTAP_ENVIRONMENT: Optional[str] = os.getenv("AZURE_OTAP_ENVIRONMENT")
 
 # [registry]/[imagename]:[tag]
-CONTAINER_IMAGE: Optional[str] = 'cvtweuacrogidgmnhwma3zq.azurecr.io/test:latest'
+CONTAINER_IMAGE: Optional[str] = 'crdavebbn1ontweu01.azurecr.io/test:latest'
 
 # Command that you want to run on container start
 COMMAND_TO_EXECUTE: list = ["sh", "tail -f /dev/null"]
@@ -42,18 +42,6 @@ with DAG(
         'retries': 1,
         'retry_delay': timedelta(minutes=5),
         'start_date': days_ago(1),
-        # 'queue': 'bash_queue',
-        # 'pool': 'backfill',
-        # 'priority_weight': 10,
-        # 'end_date': datetime(2016, 1, 1),
-        # 'wait_for_downstream': False,
-        # 'sla': timedelta(hours=2),
-        # 'execution_timeout': timedelta(seconds=300),
-        # 'on_failure_callback': some_function,
-        # 'on_success_callback': some_other_function,
-        # 'on_retry_callback': another_function,
-        # 'sla_miss_callback': yet_another_function,
-        # 'trigger_rule': 'all_success'
     },
     template_searchpath=["/"],
     catchup=False,
