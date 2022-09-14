185c3c3561a16f5f3a272cb8c99f1d8bf1b5e417
Diana <epureanudiana13@gmail.com>
2022-09-14 16:33:40 +0200

    test it altogether

--- dags/always-on-dag.py
+++ dags/always-on-dag.py
@@ -9,7 +9,8 @@ from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import (
 )
 
 # [registry]/[imagename]:[tag]
-CONTAINER_IMAGE: Optional[str] = 'cvtweuacrogidgmnhwma3zq.azurecr.io/rebuilt:latest'
+RETRIEVAL_CONTAINER_IMAGE: Optional[str] = 'cvtweuacrogidgmnhwma3zq.azurecr.io/retrieve:latest'
+BLUR_CONTAINER_IMAGE: Optional[str] = 'cvtweuacrogidgmnhwma3zq.azurecr.io/blur:latest'
 
 # Command that you want to run on container start
 DAG_ID: Final = "cvt-pipeline"
@@ -19,10 +20,10 @@ AKS_NAMESPACE: Final = os.getenv("AIRFLOW__KUBERNETES__NAMESPACE")
 AKS_NODE_POOL: Final = "cvision2work"
 
 # Command that you want to run on container start
-COMMAND_TO_EXECUTE: list = ["python"]
+#COMMAND_TO_EXECUTE: list = ["python"]
 
 # Command arguments that will be used with command to execute on start
-COMMAND_ARGS_DATA_INGEST: list = ["/opt/test.py"]
+#COMMAND_ARGS_DATA_INGEST: list = ["/opt/retrieve_images.py"]
 
 # List here all environment variables that also needs to be
 # used inside the K8PodOperator pod.
@@ -63,12 +64,12 @@ with DAG(
     retrieve_images = KubernetesPodOperator(
             task_id='retrieve_images',
             namespace=AKS_NAMESPACE,
-            image=CONTAINER_IMAGE,
+            image=RETRIEVAL_CONTAINER_IMAGE,
             # beware! If env vars are needed from worker,
             # add them here.
             env_vars=get_generic_vars(),
-            cmds=COMMAND_TO_EXECUTE,
-            arguments=COMMAND_ARGS_DATA_INGEST,
+            cmds=["python"],
+            arguments=["/opt/retrieve_images.py"],
             labels=DAG_LABEL,
             name=DAG_ID,
             # Determines when to pull a fresh image, if 'IfNotPresent' will cause
@@ -83,7 +84,7 @@ with DAG(
             in_cluster=True,  # if true uses our service account token as aviable in Airflow on K8
             is_delete_operator_pod=False,  # if true delete pod when pod reaches its final state.
             log_events_on_failure=True,  # if true log the pod’s events if a failure occurs
-            hostnetwork=True,  # If True enable host networking on the pod. Be ware, this value must be
+            hostnetwork=True,  # If True enable host networking on the pod. Beware, this value must be
             # set to true if you want to make use of the pod-identity facilities like managed identity.
             reattach_on_restart=True,
             dag=dag,
@@ -100,9 +101,34 @@ with DAG(
             volume_mounts=[],
         )
 
+    blur_images = KubernetesPodOperator(
+        task_id='retrieve_images',
+        namespace=AKS_NAMESPACE,
+        image=BLUR_CONTAINER_IMAGE,
+        env_vars=None,
+        cmds=["python"],
+        arguments=["/opt/blur.py"],
+        labels=DAG_LABEL,
+        name=DAG_ID,
+        image_pull_policy="Always",
+        get_logs=True,
+        in_cluster=True,
+        is_delete_operator_pod=False, 
+        log_events_on_failure=True, 
+        hostnetwork=True, 
+        reattach_on_restart=True,
+        dag=dag,
+        startup_timeout_seconds=3600,
+        execution_timeout=timedelta(hours=4),
+        node_selector={"nodetype": AKS_NODE_POOL},
+        volumes=[],
+        volume_mounts=[],
+    )
+    
+
 # FLOW
-(
-    retrieve_images
+var = (
+        retrieve_images >> blur_images
 )
 
 
