c5384dd10991de7ea1b30d05702205ea6521f365
Diana <epureanudiana13@gmail.com>
2022-09-16 10:16:29 +0200

    test sa

--- dags/sa_test.Dockerfile
+++ dags/sa_test.Dockerfile
@@ -1,10 +1,12 @@
+#  az acr build -t sa_test:latest -f sa_test.Dockerfile -r cvtweuacrogidgmnhwma3zq .
 FROM python:3.7.13-bullseye
 
 RUN pip install \
     azure-identity==1.10.0 \
-    azure-keyvault-secrets==4.5.1
+    azure-keyvault-secrets==4.5.1 \
+    azure-storage-blob==12.13.1
 
 WORKDIR /opt
 
 
-COPY test.py /opt
+COPY sa_test.py /opt
--- dags/sa_test.py
+++ dags/sa_test.py
@@ -1,82 +1,18 @@
 import os
-from datetime import timedelta
-from typing import Final, Optional
-from airflow.utils.dates import days_ago
 
-from airflow import DAG
-from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import (
-    KubernetesPodOperator,
-)
-
-# [registry]/[imagename]:[tag]
-IMAGE: Optional[str] = 'cvtweuacrogidgmnhwma3zq.azurecr.io/retrieve:latest'
-
-# Command that you want to run on container start
-DAG_ID: Final = "debugging"
-DATATEAM_OWNER: Final = "cvision2"
-DAG_LABEL: Final = {"team_name": DATATEAM_OWNER}
-AKS_NAMESPACE: Final = os.getenv("AIRFLOW__KUBERNETES__NAMESPACE")
-AKS_NODE_POOL: Final = "cvision2work"
-
-# List here all environment variables that also needs to be
-# used inside the K8PodOperator pod.
-GENERIC_VARS_NAMES: list = [
-    "USER_ASSIGNED_MANAGED_IDENTITY",
-    "AIRFLOW__SECRETS__BACKEND_KWARGS",
-]
+from azure.storage.blob import BlobServiceClient
+from azure.identity import ManagedIdentityCredential
 
 
-def get_generic_vars() -> dict[str, str]:
-    """Get generic environment variables all containers will need.
+client_id = os.getenv("USER_ASSIGNED_MANAGED_IDENTITY")
+credential = ManagedIdentityCredential(client_id=client_id)
 
-    Note: The K8PodOperator spins up a new node. This node needs
-        to be fed with the nessacery env vars. Its not inheriting
-        it from his big brother/sister/neutral the worker pod.
+#airflow_secrets = json.loads(os.environ["AIRFLOW__SECRETS__BACKEND_KWARGS"])
+#KVUri = airflow_secrets["vault_url"]
 
-    :returns: All (generic) environment variables that need to be included into the container.
-    """
-    GENERIC_VARS_DICT: dict[str, str] = {
-        variable: os.environ[variable] for variable in GENERIC_VARS_NAMES
-    }
-    return GENERIC_VARS_DICT
+creds = ManagedIdentityCredential(client_id)
 
-
-with DAG(
-    DAG_ID,
-    description="test-dag",
-    default_args={
-        'depends_on_past': False,
-        'email': ['airflow@example.com'],
-        'email_on_failure': False,
-        'email_on_retry': False,
-        'retries': 1,
-        'retry_delay': timedelta(minutes=5),
-        'start_date': days_ago(1),
-    },
-    template_searchpath=["/"],
-    catchup=False,
-) as dag:
-    sa_test = KubernetesPodOperator(
-            task_id='sa_test',
-            namespace=AKS_NAMESPACE,
-            image=IMAGE,
-            env_vars=get_generic_vars(),
-            cmds=["sh", "-c", "mkdir -p /airflow/xcom/;echo '[1,2,3,4]' > /airflow/xcom/return.json"],
-            labels=DAG_LABEL,
-            name=DAG_ID,
-            image_pull_policy="Always",
-            get_logs=True,
-            in_cluster=True,  # if true uses our service account token as aviable in Airflow on K8
-            is_delete_operator_pod=False,  # if true delete pod when pod reaches its final state.
-            log_events_on_failure=True,  # if true log the pod’s events if a failure occurs
-            hostnetwork=True,  # If True enable host networking on the pod. Beware, this value must be
-            # set to true if you want to make use of the pod-identity facilities like managed identity.
-            reattach_on_restart=True,
-            dag=dag,
-            startup_timeout_seconds=3600,
-            execution_timeout=timedelta(hours=4),
-            do_xcom_push=True,
-            node_selector={"nodetype": AKS_NODE_POOL},
-            volumes=[],
-            volume_mounts=[],
-        )
+blob_service_client = BlobServiceClient(account_url="https://cvtdataweuogidgmnhwma3zq.blob.core.windows.net", credential=creds)
+test = blob_service_client.list_containers()
+for container in test:
+    print(container.name)
