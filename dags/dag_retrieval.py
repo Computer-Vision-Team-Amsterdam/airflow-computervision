f70b8343d1d03d9d3a4d8ea0839f81f1b29128e8
Diana <epureanudiana13@gmail.com>
2022-09-26 15:17:06 +0200

    stupid typo

--- dags/dag_retrieval.py
+++ dags/dag_retrieval.py
@@ -49,7 +49,7 @@ airflow_secrets = json.loads(os.environ["AIRFLOW__SECRETS__BACKEND_KWARGS"])
 KVUri = airflow_secrets["vault_url"]
 
 client = SecretClient(vault_url=KVUri, credential=credential)
-username_secret = client.get_secret(name="CloudVpsRawPassword")
+username_secret = client.get_secret(name="CloudVpsRawUsername")
 password_secret = client.get_secret(name="CloudVpsRawPassword")
 socket.setdefaulttimeout(100)
 
