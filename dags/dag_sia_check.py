835872196ccbeba1ec6f36dac921e10a03a2283d
Diana <epureanudiana13@gmail.com>
2022-09-28 14:34:20 +0200

    reproduce security issue

--- dags/dag_sia_check.py
+++ dags/dag_sia_check.py
@@ -9,10 +9,6 @@ from typing import Final, Optional, Tuple
 from airflow.utils.dates import days_ago
 
 import requests
-from requests.auth import HTTPBasicAuth
-from azure.identity import ManagedIdentityCredential
-from azure.keyvault.secrets import SecretClient
-from azure.storage.blob import BlobServiceClient
 
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
@@ -24,23 +20,9 @@ DAG_LABEL: Final = {"team_name": DATATEAM_OWNER}
 AKS_NAMESPACE: Final = os.getenv("AIRFLOW__KUBERNETES__NAMESPACE")
 AKS_NODE_POOL: Final = "cvision2work"
 
-
-client_id = os.getenv("USER_ASSIGNED_MANAGED_IDENTITY")
-credential = ManagedIdentityCredential(client_id=client_id)
-blob_service_client = BlobServiceClient(account_url="https://cvtdataweuogidgmnhwma3zq.blob.core.windows.net",
-                                        credential=credential)
-
-airflow_secrets = json.loads(os.environ["AIRFLOW__SECRETS__BACKEND_KWARGS"])
-KVUri = airflow_secrets["vault_url"]
-
-client = SecretClient(vault_url=KVUri, credential=credential)
-sia_password = client.get_secret(name="sia-password-acc")
-socket.setdefaulttimeout(100)
-
-
 def check_sia_connection():
-    url = "https://acc.api.data.amsterdam.nl/signals/v1/private/users"
-    response = requests.get(url, stream=True, auth=HTTPBasicAuth("sia-cvt", sia_password))
+    url = "https://acc.api.data.amsterdam.nl/signals/v1/public/feedback/standard_answers"
+    response = requests.get(url, stream=True)
     print(f"Respose status code: {response.status_code}.")
 
 with DAG(
