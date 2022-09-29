2eb99da65a205fef7ffa001fd549759ff9de7ad2
Diana <epureanudiana13@gmail.com>
2022-09-29 14:32:03 +0200

    back to working version sia

--- dags/dag_sia_check.py
+++ dags/dag_sia_check.py
@@ -9,6 +9,9 @@ from typing import Final, Optional, Tuple
 from airflow.utils.dates import days_ago
 
 import requests
+from requests.auth import HTTPBasicAuth
+from azure.identity import ManagedIdentityCredential
+from azure.keyvault.secrets import SecretClient
 
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
@@ -20,10 +23,37 @@ DAG_LABEL: Final = {"team_name": DATATEAM_OWNER}
 AKS_NAMESPACE: Final = os.getenv("AIRFLOW__KUBERNETES__NAMESPACE")
 AKS_NODE_POOL: Final = "cvision2work"
 
+
+client_id = os.getenv("USER_ASSIGNED_MANAGED_IDENTITY")
+credential = ManagedIdentityCredential(client_id=client_id)
+
+airflow_secrets = json.loads(os.environ["AIRFLOW__SECRETS__BACKEND_KWARGS"])
+KVUri = airflow_secrets["vault_url"]
+
+client = SecretClient(vault_url=KVUri, credential=credential)
+sia_password = client.get_secret(name="sia-password-acc")
+socket.setdefaulttimeout(100)
+
+
 def check_sia_connection():
-    url = "https://acc.api.data.amsterdam.nl/signals/v1/public/feedback/standard_answers"
-    response = requests.get(url, stream=True)
-    print(f"Respose status code: {response.status_code}.")
+    data = {
+        'grant_type': 'client_credentials',
+        'client_id': 'sia-cvt',
+        'client_secret': f'{sia_password.value}',
+    }
+
+    print(data)
+    tokenURL = 'https://iam.amsterdam.nl/auth/realms/datapunt-ad-acc/protocol/openid-connect/token'
+    response = requests.post(tokenURL, data=data)
+
+    if response.status_code == 200:
+        token = response.json()["access_token"]
+        url = "https://acc.api.data.amsterdam.nl/signals/v1/private/signals"
+        headers = {'Authorization': "Bearer {}".format(token)}
+        response = requests.get(url, headers=headers)
+        print(f"Respose status SIA private endpoint: {response.status_code}.")
+    else:
+        print(f"Response status code for token {response.status_code}")
 
 with DAG(
     DAG_ID,
