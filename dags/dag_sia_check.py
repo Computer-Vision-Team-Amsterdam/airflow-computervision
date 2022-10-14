b3eb3bb35f7acee62d84d3c17abffdeb8fa32086
chrise96 <chrise96@hotmail.com>
2022-10-14 13:47:54 +0200

    Update dag_sia_check.py

--- dags/dag_sia_check.py
+++ dags/dag_sia_check.py
@@ -23,6 +23,7 @@ DAG_LABEL: Final = {"team_name": DATATEAM_OWNER}
 AKS_NAMESPACE: Final = os.getenv("AIRFLOW__KUBERNETES__NAMESPACE")
 AKS_NODE_POOL: Final = "cvision2work"
 
+BASE_URL = "https://acc.api.data.amsterdam.nl/signals/v1/private/signals"
 
 client_id = os.getenv("USER_ASSIGNED_MANAGED_IDENTITY")
 credential = ManagedIdentityCredential(client_id=client_id)
@@ -34,26 +35,78 @@ client = SecretClient(vault_url=KVUri, credential=credential)
 sia_password = client.get_secret(name="sia-password-acc")
 socket.setdefaulttimeout(100)
 
-
-def check_sia_connection():
-    data = {
-        'grant_type': 'client_credentials',
-        'client_id': 'sia-cvt',
-        'client_secret': f'{sia_password.value}',
+def to_signal(text: str, date_now, lat_lng: dict):
+    return {
+        "text": text,
+        "location": {
+            "geometrie": {
+                "type": "Point",
+                "coordinates": [lat_lng["lng"], lat_lng["lat"]]
+            }
+        },
+        "category": {
+            "sub_category": "/signals/v1/public/terms/categories/overig/sub_categories/overig"
+        },
+        "reporter": {
+            "email": "cvt@amsterdam.nl"
+        },
+        "incident_date_start": date_now.strftime("%Y-%m-%d %H:%M")
     }
 
-    print(data)
-    tokenURL = 'https://iam.amsterdam.nl/auth/realms/datapunt-ad-acc/protocol/openid-connect/token'
-    response = requests.post(tokenURL, data=data)
+def _get_access_token(client_id, client_secret):
+    token_url = 'https://iam.amsterdam.nl/auth/realms/datapunt-ad-acc/protocol/openid-connect/token'
+    payload = {
+        'client_id': client_id,
+        'client_secret': client_secret,
+        'grant_type': 'client_credentials'
+    }
+    response = requests.post(token_url, data=payload)
+    if response.status_code == 200:
+        print("The server successfully answered the request.")
+        return response.json()["access_token"]
+    else:
+        response.raise_for_status()
+
+def _get_signals_page(access_token, page):
+    if access_token is None:
+        raise Exception("Access token cannot be None")
+
+    headers = {'Authorization': "Bearer {}".format(access_token)}
+    url = BASE_URL + page
+    response = requests.get(BASE_URL, headers=headers)
 
     if response.status_code == 200:
-        token = response.json()["access_token"]
-        url = "https://acc.api.data.amsterdam.nl/signals/v1/private/signals"
-        headers = {'Authorization': "Bearer {}".format(token)}
-        response = requests.get(url, headers=headers)
-        print(f"Respose status SIA private endpoint: {response.status_code}.")
+        return response.json()
     else:
-        print(f"Response status code for token {response.status_code}")
+        return response.raise_for_status()
+
+def _post_signal(access_token):
+    if access_token is None:
+        raise Exception("Access token cannot be None")
+
+    headers = {'Authorization': "Bearer {}".format(access_token)}
+
+    text = "CVT Dit is een automatisch gegenereerd signaal."
+    date_now: datetime = datetime.now()
+    lat_lng = {"lat": 52.367527, "lng": 4.901257}  # TODO
+
+    response = requests.post(
+        BASE_URL,
+        json=to_signal(text, date_now, lat_lng),
+        headers=headers
+    )
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        return response.raise_for_status()
+
+def check_sia_connection():
+    access_token = _get_access_token("sia-cvt", f"{sia_password.value}")
+    print(_get_signals_page(access_token, "?page_size=1"))
+
+    print(_post_signal(access_token))
+
 
 with DAG(
     DAG_ID,
