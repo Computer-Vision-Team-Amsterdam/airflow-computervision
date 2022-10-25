908b8f2027175dba2866ea58414db960cbb2b93b
chrise96 <chrise96@hotmail.com>
2022-10-25 14:12:31 +0200

    Use docker

--- dags/dag_sia_check.py
+++ dags/dag_sia_check.py
@@ -1,117 +1,33 @@
-import json
-import os
 from datetime import timedelta
-import shutil
-import socket
-from datetime import datetime
-from pathlib import Path
-from typing import Final, Optional, Tuple
+from typing import Final, Optional
+import os
 from airflow.utils.dates import days_ago
-
-import requests
-from requests.auth import HTTPBasicAuth
-from azure.identity import ManagedIdentityCredential
-from azure.keyvault.secrets import SecretClient
-
 from airflow import DAG
-from airflow.operators.python_operator import PythonOperator
+from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import (
+    KubernetesPodOperator,
+)
 
-# Command that you want to run on container start
+IMAGE: Optional[str] = 'cvtweuacrogidgmnhwma3zq.azurecr.io/sia:latest'
+
 DAG_ID: Final = "sia"
 DATATEAM_OWNER: Final = "cvision2"
 DAG_LABEL: Final = {"team_name": DATATEAM_OWNER}
 AKS_NAMESPACE: Final = os.getenv("AIRFLOW__KUBERNETES__NAMESPACE")
 AKS_NODE_POOL: Final = "cvision2work"
 
-BASE_URL = "https://acc.api.data.amsterdam.nl/signals/v1/private/signals"
+def get_generic_vars() -> dict[str, str]:
+    """Get generic environment variables all containers will need.
 
-client_id = os.getenv("USER_ASSIGNED_MANAGED_IDENTITY")
-credential = ManagedIdentityCredential(client_id=client_id)
+    Note: The K8PodOperator spins up a new node. This node needs
+        to be fed with the nessacery env vars. Its not inheriting
+        it from his big brother/sister/neutral the worker pod.
 
-airflow_secrets = json.loads(os.environ["AIRFLOW__SECRETS__BACKEND_KWARGS"])
-KVUri = airflow_secrets["vault_url"]
-
-client = SecretClient(vault_url=KVUri, credential=credential)
-sia_password = client.get_secret(name="sia-password-acc")
-socket.setdefaulttimeout(100)
-
-def to_signal(text: str, date_now, lat_lng: dict):
-    return {
-        "text": text,
-        "location": {
-            "geometrie": {
-                "type": "Point",
-                "coordinates": [lat_lng["lng"], lat_lng["lat"]]
-            }
-        },
-        "category": {
-            "sub_category": "signals/v1/public/terms/categories/overlast-in-de-openbare-ruimte/sub_categories/hinderlijk-geplaatst-object"
-        },
-        "reporter": {
-            "email": "cvt@amsterdam.nl"
-        },
-        "priority": {
-            "priority": "low",
-        },
-        "incident_date_start": date_now.strftime("%Y-%m-%d %H:%M")
+    :returns: All (generic) environment variables that need to be included into the container.
+    """
+    GENERIC_VARS_DICT: dict[str, str] = {
+        variable: os.environ[variable] for variable in GENERIC_VARS_NAMES
     }
-
-def _get_access_token(client_id, client_secret):
-    token_url = 'https://iam.amsterdam.nl/auth/realms/datapunt-ad-acc/protocol/openid-connect/token'
-    payload = {
-        'client_id': client_id,
-        'client_secret': client_secret,
-        'grant_type': 'client_credentials'
-    }
-    response = requests.post(token_url, data=payload)
-    if response.status_code == 200:
-        print("The server successfully answered the request.")
-        return response.json()["access_token"]
-    else:
-        response.raise_for_status()
-
-def _get_signals_page(access_token, page):
-    if access_token is None:
-        raise Exception("Access token cannot be None")
-
-    headers = {'Authorization': "Bearer {}".format(access_token)}
-    url = BASE_URL + page
-    response = requests.get(BASE_URL, headers=headers)
-
-    if response.status_code == 200:
-        print("The server successfully performed the GET request.")
-        return response.json()
-    else:
-        return response.raise_for_status()
-
-def _post_signal(access_token):
-    if access_token is None:
-        raise Exception("Access token cannot be None")
-
-    headers = {'Authorization': "Bearer {}".format(access_token)}
-
-    text = "CVT Dit is een automatisch gegenereerd signaal."
-    date_now: datetime = datetime.now()
-    lat_lng = {"lat": 52.367527, "lng": 4.901257}  # TODO
-
-    response = requests.post(
-        BASE_URL,
-        json=to_signal(text, date_now, lat_lng),
-        headers=headers
-    )
-
-    if response.status_code == 200:
-        print("The server successfully performed the POST request.")
-        return response.json()
-    else:
-        return response.raise_for_status()
-
-def check_sia_connection():
-    access_token = _get_access_token("sia-cvt", f"{sia_password.value}")
-    # print(_get_signals_page(access_token, "?page_size=1"))
-
-    # print(_post_signal(access_token))
-
+    return GENERIC_VARS_DICT
 
 with DAG(
     DAG_ID,
@@ -128,12 +44,30 @@ with DAG(
     template_searchpath=["/"],
     catchup=False,
 ) as dag:
-
-    test_sia = PythonOperator(task_id='test_sia',
-                              python_callable=check_sia_connection,
-                              provide_context=True,
-                              dag=dag)
-
+    test_sia = KubernetesPodOperator(
+            task_id='test_sia',
+            namespace=AKS_NAMESPACE,
+            image=IMAGE,
+            env_vars=get_generic_vars(),
+            cmds=["python"],
+            arguments=["/app/submit_to_sia.py"],
+            labels=DAG_LABEL,
+            name=DAG_ID,
+            image_pull_policy="Always",
+            get_logs=True,
+            in_cluster=True,  # if true uses our service account token as aviable in Airflow on K8
+            is_delete_operator_pod=False,  # if true delete pod when pod reaches its final state.
+            log_events_on_failure=True,  # if true log the pod’s events if a failure occurs
+            hostnetwork=True,  # If True enable host networking on the pod. Beware, this value must be
+            # set to true if you want to make use of the pod-identity facilities like managed identity.
+            reattach_on_restart=True,
+            dag=dag,
+            startup_timeout_seconds=3600,
+            execution_timeout=timedelta(hours=4),
+            node_selector={"nodetype": AKS_NODE_POOL},
+            volumes=[],
+            volume_mounts=[],
+        )
 
 # FLOW
 var = (
