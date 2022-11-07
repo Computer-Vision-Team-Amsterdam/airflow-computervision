5164e3d514da9a08fbebebc85191c0f3f5693cea
chrise96 <chrise96@hotmail.com>
2022-11-07 12:00:08 +0100

    Add SIA part

--- dags/pipeline.py
+++ dags/pipeline.py
@@ -19,6 +19,7 @@ BLUR_CONTAINER_IMAGE: Optional[str] = 'cvtweuacrogidgmnhwma3zq.azurecr.io/blur:l
 DETECT_CONTAINER_IMAGE: Optional[str] = 'cvtweuacrogidgmnhwma3zq.azurecr.io/detection:latest'
 POSTPROCESSING_CONTAINER_IMAGE: Optional[str] = 'cvtweuacrogidgmnhwma3zq.azurecr.io/postprocessing:latest'
 UPLOAD_TO_POSTGRES_CONTAINER_IMAGE: Optional[str] = 'cvtweuacrogidgmnhwma3zq.azurecr.io/upload_to_postgres:latest'
+SIA_IMAGE: Optional[str] = 'cvtweuacrogidgmnhwma3zq.azurecr.io/sia:latest'
 DATE = '{{dag_run.conf["date"]}}'  # set in config when triggering DAG
 
 # Command that you want to run on container start
@@ -273,11 +274,36 @@ with DAG(
             volume_mounts=[],
         )
 
+    add_sia = KubernetesPodOperator(
+        task_id='test_sia',
+        namespace=AKS_NAMESPACE,
+        image=SIA_IMAGE,
+        env_vars=get_generic_vars(),
+        cmds=["python"],
+        arguments=["/app/submit_to_sia.py",
+                   "--date", DATE],
+        labels=DAG_LABEL,
+        name=DAG_ID,
+        image_pull_policy="Always",
+        get_logs=True,
+        in_cluster=True,  # if true uses our service account token as aviable in Airflow on K8
+        is_delete_operator_pod=False,  # if true delete pod when pod reaches its final state.
+        log_events_on_failure=True,  # if true log the pod’s events if a failure occurs
+        hostnetwork=True,  # If True enable host networking on the pod. Beware, this value must be
+        # set to true if you want to make use of the pod-identity facilities like managed identity.
+        reattach_on_restart=True,
+        dag=dag,
+        startup_timeout_seconds=3600,
+        execution_timeout=timedelta(hours=4),
+        node_selector={"nodetype": AKS_NODE_POOL},
+        volumes=[],
+        volume_mounts=[],
+    )
 
 # FLOW
 
     flow = retrieve_images >> [blur_images, store_images_metadata] >> remove_unblurred_images >> \
-           detect_containers  >> store_detections >> postprocessing
+           detect_containers  >> store_detections >> postprocessing >> add_sia
 
 
 
