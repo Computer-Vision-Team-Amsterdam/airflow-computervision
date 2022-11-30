e6a10d5ac4a8b305a4cf31097d68f0c4b7bafa26
chrise96 <chrise96@hotmail.com>
2022-11-30 11:41:54 +0100

    Test decos

--- dags/dag.py
+++ dags/dag.py
@@ -1,7 +1,56 @@
 from datetime import datetime
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
+from airflow.operators.python_operator import PythonOperator
+from airflow.models import Variable
+
+try:
+    username = Variable.get("DecosUsername")
+    password = Variable.get("DecosPassword")
+    url = Variable.get("DecosURL")
+except Exception as e:
+    print(e)
+
+
+def login_test(**context):
+    values = {'username': username,
+              'password': password}
+
+    try:
+        import requests
+        r = requests.post(url, data=values)
+        print(r.content)
+    except Exception as e:
+        print(e)
+
+def login_test_twee(**context):
+    try:
+        import requests
+        page = requests.get(url)
+        print(page.text.encode('utf8'))
+    except Exception as e:
+        print(e)
+
 
 with DAG(dag_id='bash_dag', schedule_interval=None, start_date=datetime(2022, 11, 24), catchup=False) as dag:
-  BashOperator(task_id='bash_task', bash_command="echo -e '\x1dclose\x0d' | telnet sfte.amsterdam.nl 22")
- 
+    telnet_test = BashOperator(
+        task_id='bash_task',
+        bash_command="echo -e '\x1dclose\x0d' | telnet sfte.amsterdam.nl 22",
+        dag=dag
+    )
+
+    login_test = PythonOperator(
+        task_id='login_test',
+        python_callable=login_test,
+        provide_context=True,
+        dag=dag
+    )
+
+    login_test_twee = PythonOperator(
+        task_id='login_test_twee',
+        python_callable=login_test_twee,
+        provide_context=True,
+        dag=dag
+    )
+
+    flow = telnet_test >> login_test >> login_test_twee
