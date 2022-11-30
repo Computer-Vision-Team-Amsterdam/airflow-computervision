ae96025c7a5ba5025f9e604cdb2d76b7472d283b
Chris Eijgenstein <chrise96@hotmail.com>
2022-11-30 15:17:18 +0100

    Update dag.py (#14)
    * Update dag.py
    
    * Update dag.py

--- dags/dag.py
+++ dags/dag.py
@@ -32,11 +32,29 @@ def login_test(**context):
 def login_test_twee(**context):
     try:
         import requests
-        page = requests.get(url)
+        from urllib3.exceptions import InsecureRequestWarning
+        from urllib3 import disable_warnings
+
+        disable_warnings(InsecureRequestWarning)
+        page = requests.get(url, verify=False)
         print(page.text.encode('utf8'))
+
+        print(page.content)
     except Exception as e:
         print(e)
 
+def login_test_drie(**context):
+    try:
+        from urllib.request import urlopen
+        print(urlopen(url).read())
+
+        import requests
+        page = requests.get(url, verify=False)
+        print(page.text.encode('utf8'))
+
+        print(page.content)
+    except Exception as e:
+        print(e)
 
 with DAG(dag_id='bash_dag', schedule_interval=None, start_date=datetime(2022, 11, 24), catchup=False) as dag:
     telnet_test = BashOperator(
@@ -59,4 +77,11 @@ with DAG(dag_id='bash_dag', schedule_interval=None, start_date=datetime(2022, 11
         dag=dag
     )
 
-    flow = telnet_test >> login_test >> login_test_twee
+    login_test_drie = PythonOperator(
+        task_id='login_test_drie',
+        python_callable=login_test_drie,
+        provide_context=True,
+        dag=dag
+    )
+
+    flow = telnet_test >> login_test >> login_test_twee >> login_test_drie
