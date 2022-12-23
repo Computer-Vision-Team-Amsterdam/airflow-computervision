73e7a9a527ee7c644759781ef38e2938f4cc3dd0
chrise96 <chrise96@hotmail.com>
2022-12-23 10:30:20 +0100

    Terug

--- dags/test_dag_multiprocessing.py
+++ dags/test_dag_multiprocessing.py
@@ -5,13 +5,13 @@ import uuid
 from datetime import timedelta
 
 from airflow import DAG
-from airflow.decorators import task
 from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
 from airflow.utils.dates import days_ago
 from airflow.utils.task_group import TaskGroup
 
 
+WORKER_COUNT = 5
 TASKS = [f"{str(uuid.uuid4())}.jpg" for _ in range(100)]
 
 
@@ -37,26 +37,15 @@ with DAG(
         template_searchpath=["/"],
         catchup=False,
 ) as dag:
-    @task
-    def get_config(**context):
-        workers = context['dag_run'].conf.get('workers', default=None)
-        print(workers)
-        print(int(workers))
-        print(workers.isdigit())
-        print("Default to 1 worker.")
-        return 1
-
-    NUM_WORKERS = get_config()
-
     start = BashOperator(task_id="start", bash_command="echo Starting DAG")
     collect = BashOperator(task_id="collect", bash_command="echo Collecting multiprocessing results")
     end = BashOperator(task_id="end", bash_command="echo Ending DAG")
 
     blur_tasks = [
-        PythonOperator(task_id=f"multiprocessing_blur_{i}", python_callable=process_task) for i in range(NUM_WORKERS)]
+        PythonOperator(task_id=f"multiprocessing_blur_{i}", python_callable=process_task) for i in range(WORKER_COUNT)]
 
     with TaskGroup("multiprocessing_detection") as multiprocessing_detection:
-        for i in range(NUM_WORKERS):
+        for i in range(WORKER_COUNT):
             PythonOperator(task_id=f"multiprocessing_detection_{i}", python_callable=process_task)
 
-    start >> blur_tasks >> collect >> multiprocessing_detection >> end
+    start >> blur_tasks  >> collect >> multiprocessing_detection >> end
