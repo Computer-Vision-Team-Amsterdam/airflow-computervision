e851a4c1e7bc7ee4ddc45480c5829a95bcd5b7cd
Diana <epureanudiana13@gmail.com>
2022-12-29 13:15:13 +0100

    separated postprocessing

--- dags/pipeline_small_multiprocessing.py
+++ dags/pipeline_small_multiprocessing.py
@@ -235,33 +235,7 @@ with DAG(
         )
         for worker_id in range(1, NUM_WORKERS+1)]
 
-    postprocessing = KubernetesPodOperator(
-        task_id='postprocessing',
-        namespace=AKS_NAMESPACE,
-        image=POSTPROCESSING_CONTAINER_IMAGE,
-        env_vars=get_generic_vars(),
-        cmds=["python"],
-        arguments=["/app/postprocessing.py",
-                   "--date", DATE],
-        labels=DAG_LABEL,
-        name=DAG_ID,
-        image_pull_policy="Always",
-        get_logs=True,
-        in_cluster=True,  # if true uses our service account token as aviable in Airflow on K8
-        is_delete_operator_pod=True,  # if true delete pod when pod reaches its final state.
-        log_events_on_failure=True,  # if true log the pod’s events if a failure occurs
-        hostnetwork=True,  # If True enable host networking on the pod. Beware, this value must be
-        # set to true if you want to make use of the pod-identity facilities like managed identity.
-        reattach_on_restart=True,
-        dag=dag,
-        startup_timeout_seconds=3600,
-        execution_timeout=timedelta(hours=4),
-        node_selector={"nodetype": AKS_NODE_POOL},
-        volumes=[],
-        volume_mounts=[],
-    )
-
     # FLOW
 
     flow = retrieve_images >> store_images_metadata >> blur_tasks >> remove_unblurred_images >> \
-           detect_containers_tasks >> postprocessing
+           detect_containers_tasks
