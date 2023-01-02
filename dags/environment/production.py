9d869ccbd073a9894db3146eb5378b1cd12451ed
Thomas Brockmeier <t.brockmeier@amsterdam.nl>
2023-01-02 15:26:39 +0100

    adds file with production settings

---
+++ dags/environment/production.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+
+ACR_URL = "cvtweuacrpju7nekr55bcdo.azurecr.io"
+BLOB_URL = "https://cvtdataweupju7nekr55bcdo.blob.core.windows.net"
+
+# [regiPathy]/[imagename]:[tag]
+BLUR_CONTAINER_IMAGE = ACR_URL + "/blur:latest"
+DELETE_BLOBS_IMAGE = ACR_URL + "/delete_blobs:latest"
+DETECT_CONTAINER_IMAGE = ACR_URL + "/detection:latest"
+POSTPROCESSING_CONTAINER_IMAGE = ACR_URL + "/postprocessing:latest"
+RETRIEVAL_CONTAINER_IMAGE = ACR_URL + "/retrieve:latest"
+SUBMIT_TO_SIA_IMAGE = ACR_URL + "/submit_to_sia:latest"
+UPLOAD_TO_POSTGRES_CONTAINER_IMAGE = ACR_URL + "/upload_to_postgres:latest"
