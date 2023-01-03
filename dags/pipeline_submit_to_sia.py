e60fc3e226f44022b31d155e3fa9197519a9166f
chrise96 <chrise96@hotmail.com>
2023-01-03 13:11:41 +0100

    Cleanup

--- dags/pipeline_submit_to_sia.py
+++ dags/pipeline_submit_to_sia.py
@@ -56,7 +56,7 @@ with DAG(
             'email': ['airflow@example.com'],
             'email_on_failure': False,
             'email_on_retry': False,
-            'retries': 1,
+            'retries': 0,
             'retry_delay': timedelta(minutes=5),
             'start_date': days_ago(1),
             'schedule_interval': None,
