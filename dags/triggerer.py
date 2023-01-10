910d5a8bf5fe1b556652d90cb0620d690d3d8ad0
Thomas Brockmeier <t.brockmeier@amsterdam.nl>
2023-01-10 15:47:55 +0100

    returns list

--- dags/triggerer.py
+++ dags/triggerer.py
@@ -21,7 +21,7 @@ def trigger_generator(**ctx):
     )
     trigger_times = [first_run + timedelta(hours=x * 2) for x in range(n_runs)]
     arguments = [first_run + timedelta(minutes=x) for x in range(n_runs)]
-    return zip(trigger_times, arguments)
+    return list(zip(trigger_times, arguments))
 
 
 with DAG(
