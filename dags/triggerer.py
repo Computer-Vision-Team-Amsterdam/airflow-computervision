fe3a7daf8710c77f1edc76af9a2b729e118b1384
Thomas Brockmeier <t.brockmeier@amsterdam.nl>
2023-01-10 20:18:48 +0100

    wait for completion?

--- dags/triggerer.py
+++ dags/triggerer.py
@@ -28,7 +28,7 @@ with DAG(
     "trigger-dagrun-dag",
     start_date=datetime(2023, 1, 9),
     max_active_runs=1,
-    schedule_interval="0 21 * * 1-5",
+    schedule_interval="0 22 * * 1-5",
     default_args=default_args,
     catchup=False
 ) as dag:
@@ -36,7 +36,7 @@ with DAG(
         TriggerDagRunOperator(
             task_id=f"trigger_dependent_dag_{x}",
             trigger_dag_id="dependent",
-            wait_for_completion=False,
+            wait_for_completion=True,
             execution_date=f"{{{{ data_interval_start.add(hours={x} * 2) }}}}",
             conf={"date": f"{{{{ ds ~ ' 21:{x}0:00' }}}}"},
         )
