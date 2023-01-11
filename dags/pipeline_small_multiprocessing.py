3b69befa5f31e3161dfc22482dc18298d34c935b
Thomas Brockmeier <t.brockmeier@amsterdam.nl>
2023-01-11 15:34:23 +0100

    formatting, scheduler update

--- dags/pipeline_small_multiprocessing.py
+++ dags/pipeline_small_multiprocessing.py
@@ -111,7 +111,7 @@ with DAG(
         "trigger-multiprocessing-small",
         start_date=datetime(2023, 1, 10),
         max_active_runs=1,
-        schedule_interval="0 21 * * 3",  # Every Wednesday at 21:00 CE(S)T
+        schedule_interval="0 20 * * 3",  # Will start every Wednesday at 21:00 CE(S)T
         default_args=default_args,
         catchup=False
 ) as dag:
@@ -123,7 +123,7 @@ with DAG(
             # creates (4) DAGRuns for the triggered DAG at 2 hour intervals from (and including) the current DAG's start time
             # data_interval_end is the moment at which the DAG is scheduled to start _in UTC_, i.e., 21:00 CE(S)T, converted to UTC
             execution_date=f"{{{{ data_interval_end.in_tz('Europe/Amsterdam').add(hours={x} * 2) }}}}",  # data_interval_end is in UTC
-            conf={"date": f"{{{{ data_interval_end.in_tz('Europe/Amsterdam').to_date_string() ~ ' 21:{x}0:00' }}}}"},
+            conf={"date": f"{{{{ data_interval_end.in_tz('Europe/Amsterdam').to_date_string() ~ ' 21:{x}0:00.00' }}}}"},
         )
         for x in range(4)]
 
