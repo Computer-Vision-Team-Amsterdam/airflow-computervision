00ebcd8913cb5568aae61e1e3528aa5fbcde5fb5
Thomas Brockmeier <t.brockmeier@amsterdam.nl>
2023-01-11 14:13:01 +0100

    typo

--- dags/trigger-test.py
+++ dags/trigger-test.py
@@ -40,7 +40,7 @@ with DAG(
     start_date=datetime(2023, 1, 9),
     max_active_runs=1,
     schedule_interval="0/5 * * * 3",  # Wednesday, every 5 mins
-    # schedule_interval="0 22 * * 3",  # Every Wednesday at 21:00 CET
+    # schedule_interval="0 21 * * 3",  # Every Wednesday at 21:00 CET
     default_args=default_args,
     catchup=False
 ) as dag:
