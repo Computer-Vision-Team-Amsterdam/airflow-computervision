2b874500448ebb876f8868b83c7e96497c7efcd2
Thomas Brockmeier <t.brockmeier@amsterdam.nl>
2023-01-18 15:32:48 +0100

    tries accessing kubeapi

---
+++ dags/kubeapi.py
@@ -0,0 +1,28 @@
+from airflow import DAG
+from airflow.operators.python_operator import PythonOperator
+
+from common import default_args
+
+
+def script():
+    from kubernetes import client, config
+
+    config.load_kube_config()
+
+    v1 = client.CoreV1Api()
+    print("Listing pods with their IPs:")
+    ret = v1.list_pod_for_all_namespaces(watch=False)
+    for i in ret.items:
+        print("%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
+
+
+with DAG(
+        "kubeapi",
+        default_args=default_args,
+        description="Test Kubeapi access",
+        schedule_interval=None,
+) as dag:
+    task1 = PythonOperator(
+        task_id="send_slack_message",
+        python_callable=script,
+    )
