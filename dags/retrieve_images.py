75459c9723ed54de3369a25a05e54c1a0a02aab6
Thomas Brockmeier <t.brockmeier@amsterdam.nl>
2023-01-24 15:36:56 +0100

    extra test dasg

--- dags/retrieve_images.py
+++ dags/retrieve_images.py
@@ -142,3 +142,57 @@ with DAG(
 
     # FLOW
     flow = retrieve_images
+
+with DAG(
+        DAG_ID,
+        start_date=datetime(2023, 1, 1),
+        description="retrieve-images-2",
+        default_args=default_args,
+        on_failure_callback=on_failure_callback,
+        on_success_callback=on_success_callback,
+        schedule_interval=None,
+        template_searchpath=["/"],
+        catchup=False,
+) as dag:
+    retrieve_images = KubernetesPodOperator(
+        task_id='retrieve_images',
+        namespace=AKS_NAMESPACE,
+        image="cvtweuacrogidgmnhwma3zq.azurecr.io/retrieve_images2:latest",
+        # beware! If env vars are needed from worker,
+        # add them here.
+        env_vars=get_generic_vars(),
+        cmds=["bash", "/opt/retrieve_images.sh"],
+        arguments=[DATE],
+        labels=DAG_LABEL,
+        name=DAG_ID,
+        # Determines when to pull a fresh image, if 'IfNotPresent' will cause
+        # the Kubelet to skip pulling an image if it already exists. If you
+        # want to always pull a new image, set it to 'Always'.
+        image_pull_policy="Always",
+        # Known issue in the KubernetesPodOperator
+        # https://stackoverflow.com/questions/55176707/airflow-worker-connection-broken-incompleteread0-bytes-read
+        # set get_logs to false
+        # If true, logs stdout output of container. Defaults to True.
+        get_logs=True,
+        in_cluster=True,  # if true uses our service account token as aviable in Airflow on K8
+        is_delete_operator_pod=True,  # if true delete pod when pod reaches its final state.
+        log_events_on_failure=True,  # if true log the pod’s events if a failure occurs
+        hostnetwork=True,  # If True enable host networking on the pod. Beware, this value must be
+        # set to true if you want to make use of the pod-identity facilities like managed identity.
+        reattach_on_restart=True,
+        dag=dag,
+        # Timeout to start up the Pod, default is 120.
+        startup_timeout_seconds=3600,
+        # to prevent tasks becoming marked as failed when taking longer
+        # and deleting them if staling
+        execution_timeout=timedelta(hours=4),
+        # Select a specific nodepool to use. Could also be specified by nodeAffinity.
+        node_selector={"nodetype": AKS_NODE_POOL},
+        # List of Volume objects to pass to the Pod.
+        volumes=[],
+        # List of VolumeMount objects to pass to the Pod.
+        volume_mounts=[],
+    )
+
+    # FLOW
+    flow = retrieve_images
