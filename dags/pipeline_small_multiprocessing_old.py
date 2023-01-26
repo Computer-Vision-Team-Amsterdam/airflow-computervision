5f0ce48706dcdd352df7b1aafb21b288a1b9e073
chrise96 <chrise96@hotmail.com>
2023-01-26 15:58:33 +0100

    Update pipeline_small_multiprocessing_old.py

--- dags/pipeline_small_multiprocessing_old.py
+++ dags/pipeline_small_multiprocessing_old.py
@@ -109,28 +109,6 @@ default_args = {
 }
 
 with DAG(
-        "trigger-multiprocessing-small",
-        start_date=datetime(2023, 1, 1),
-        max_active_runs=1,
-        schedule_interval="0 20 * * 3",  # 20:00 UTC, will start every Wednesday at 21:00 CET
-        default_args=default_args,
-        catchup=False
-) as dag:
-    trigs = [
-        TriggerDagRunOperator(
-            task_id=f"trigger_dependent_dag_{x}",
-            trigger_dag_id=DAG_ID,
-            wait_for_completion=True,
-            # creates (4) DAGRuns for the triggered DAG at 2 hour intervals from (and including) the current DAG's start time
-            # data_interval_end is the moment at which the DAG is scheduled to start in UTC, i.e., 21:00 CET, converted to UTC
-            execution_date=f"{{{{ data_interval_end.in_tz('Europe/Amsterdam').add(hours=1{x}) }}}}",  # data_interval_end is in UTC
-            # execution_date=f"{{{{ data_interval_end.in_tz('Europe/Amsterdam').add(hours={x}) }}}}",  # data_interval_end is in UTC
-            conf={"date": f"2022-12-31 21:0{x}:00.00"},
-            # conf={"date": f"{{{{ data_interval_end.in_tz('Europe/Amsterdam').to_date_string() ~ ' 21:{x}0:00.00' }}}}"},
-        )
-        for x in range(10)]
-
-with DAG(
         DAG_ID,
         start_date=datetime(2023, 1, 1),
         description="test-dag",
